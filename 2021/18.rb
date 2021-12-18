31714efe56f2d5e5b04b6c63c1fc9d8cfe0ece7e
Mads Ohm Larsen <mads.ohm@gmail.com>
2021-12-18 17:36:17 +0100

    Day 18

---
+++ 2021/18.rb
@@ -0,0 +1,288 @@
+data = <<HERE
+[2,[0,[9,[5,9]]]]
+[[2,[1,8]],3]
+[[[[7,2],6],[[7,8],3]],[9,[[6,9],2]]]
+[[[[7,2],[9,8]],7],[4,[[2,2],[5,0]]]]
+[[8,[2,2]],[5,[9,[4,9]]]]
+[[[[6,2],[4,8]],5],0]
+[[3,[3,[6,6]]],[6,9]]
+[[[9,5],[[8,2],[4,0]]],[[5,5],[[5,0],[1,9]]]]
+[[[[7,4],[8,1]],[2,[7,1]]],2]
+[[[[9,6],3],8],[[[9,8],7],[5,[0,8]]]]
+[[[4,[4,0]],[[7,3],3]],[8,[3,[8,2]]]]
+[[[[8,4],1],6],[[1,[8,7]],1]]
+[[[8,2],[[1,4],3]],[[4,5],[[9,1],[7,2]]]]
+[[[[5,0],[8,8]],[[4,2],4]],[2,[[4,3],[3,7]]]]
+[[[8,7],[2,1]],[9,3]]
+[[3,[7,4]],[0,3]]
+[4,[[[5,0],[5,2]],3]]
+[[[[0,1],0],8],[6,3]]
+[[7,[[9,8],[2,7]]],[[[8,8],[9,4]],[[0,5],[4,1]]]]
+[[[[3,7],[5,4]],[8,[1,8]]],[[1,8],[[6,9],9]]]
+[[[[7,4],[7,7]],7],[1,[[8,2],[1,8]]]]
+[[[[6,2],8],[[1,2],3]],[[[3,6],[4,9]],[[3,1],[9,8]]]]
+[[[3,[1,1]],[[6,5],[2,2]]],9]
+[[[[9,1],4],1],[[[1,3],3],[0,[1,4]]]]
+[[[5,0],[4,[6,8]]],[[2,4],[[0,3],[2,6]]]]
+[9,[[9,[1,5]],1]]
+[[1,[[6,0],[9,2]]],[[[4,2],7],[[2,9],6]]]
+[[[[8,2],8],9],[[[4,9],[3,8]],2]]
+[[[9,1],[6,5]],[[[9,5],5],1]]
+[[[[1,3],5],2],[1,1]]
+[[[[0,0],[8,1]],8],8]
+[[[[3,3],5],[[9,6],9]],[[3,[0,9]],7]]
+[[[6,5],1],1]
+[[[4,[1,3]],[[2,2],2]],[[8,0],[[8,1],[2,6]]]]
+[9,[[4,6],2]]
+[[[5,[8,8]],[[1,8],[4,9]]],[9,[3,6]]]
+[[[[9,3],3],0],8]
+[[[5,0],[[2,8],[1,1]]],[[[5,6],9],8]]
+[[[[5,0],[5,2]],[[7,0],[9,8]]],[3,[[5,7],[5,9]]]]
+[[3,[5,7]],1]
+[[[[2,5],[0,7]],9],[[[3,2],1],[7,1]]]
+[6,[7,[6,0]]]
+[[[8,5],[[1,7],[7,6]]],[[1,3],[5,[1,9]]]]
+[[[[9,4],[8,3]],1],[[1,6],[[2,5],1]]]
+[[[[6,5],[6,6]],[5,5]],[1,8]]
+[[[[7,7],[2,2]],3],[1,[[8,6],[5,1]]]]
+[[6,[2,4]],[[8,8],[[3,5],6]]]
+[[1,[[6,1],[9,3]]],[[2,0],5]]
+[[[5,9],[6,[1,9]]],[3,[4,[7,7]]]]
+[[[[3,6],[8,5]],[[9,4],[4,1]]],[3,3]]
+[[[3,9],[1,6]],2]
+[[[[0,9],7],6],[7,[9,[9,9]]]]
+[[[5,[6,0]],[8,[7,5]]],[[[8,8],0],[8,1]]]
+[[[[6,9],[9,0]],2],[[[0,3],[1,6]],[2,4]]]
+[[[[8,2],[3,0]],[[3,8],8]],[6,[[9,3],4]]]
+[[[6,6],2],[5,[1,4]]]
+[[1,[1,4]],[[[4,3],0],1]]
+[[[[9,9],3],0],[[[3,3],[2,8]],[1,0]]]
+[[[[1,1],[3,5]],[9,7]],4]
+[[[9,[3,6]],5],[[4,9],[9,3]]]
+[[8,7],[5,[7,[7,7]]]]
+[[[[0,5],[7,3]],[[8,6],8]],[[[4,4],[5,0]],[[2,2],2]]]
+[[[5,0],[[1,9],[5,8]]],[[1,5],[[9,3],[0,7]]]]
+[[[1,[1,5]],[8,[2,2]]],0]
+[[[6,[7,8]],[[0,2],5]],[3,[5,[8,0]]]]
+[[[[1,7],2],3],[[[8,7],[7,8]],[7,[5,5]]]]
+[[1,[7,[3,3]]],[8,[9,[3,0]]]]
+[[5,6],[[5,[2,8]],[[5,5],[8,8]]]]
+[[8,[[7,7],[4,0]]],[[5,[0,4]],[6,[6,2]]]]
+[[4,[[0,0],[0,1]]],[[3,1],[[6,7],4]]]
+[[[[3,2],[4,2]],[[4,4],[6,3]]],[9,[0,[1,9]]]]
+[[[[4,6],2],[[9,6],4]],[[9,[9,1]],[0,[1,8]]]]
+[[[5,8],[[6,5],[0,4]]],[[0,[6,3]],[2,0]]]
+[[6,8],[[5,5],[5,8]]]
+[[[7,3],[8,[6,7]]],[[[1,5],2],7]]
+[[6,[8,[8,9]]],[[[1,1],[3,0]],[[7,2],[3,7]]]]
+[[[[8,1],6],[9,[5,1]]],[[[5,9],[1,9]],5]]
+[[[[3,6],[5,7]],[[0,3],8]],[3,[[2,1],0]]]
+[[7,[5,1]],[[[3,6],9],[[4,0],6]]]
+[[[[3,8],8],0],[[1,[1,4]],[[4,5],[8,5]]]]
+[[[8,[0,6]],[4,3]],[8,[[1,5],8]]]
+[2,[[1,[9,7]],[[2,0],6]]]
+[[[[7,4],4],[[4,9],3]],[[[6,5],[0,5]],[[9,8],[2,6]]]]
+[[[3,[7,2]],[[7,7],4]],[[[3,4],[6,0]],[6,3]]]
+[[[1,9],[[9,8],9]],5]
+[[[4,2],2],[[[4,4],7],5]]
+[[[9,1],[2,[1,5]]],[[4,3],[4,[9,5]]]]
+[2,[[[8,4],1],[[2,4],2]]]
+[[[0,6],5],[1,[[2,0],6]]]
+[[[[2,4],[1,7]],[1,0]],[9,5]]
+[[7,[3,[2,0]]],[[7,8],8]]
+[[9,[1,0]],[[0,4],[[0,1],0]]]
+[0,9]
+[[[[2,9],[2,4]],[[5,6],8]],[[5,[1,4]],[3,[0,6]]]]
+[[5,[[5,8],0]],[[[0,6],[4,5]],[[8,9],[8,3]]]]
+[[[[5,2],[7,7]],[0,[4,1]]],[[8,7],[[5,3],7]]]
+[[[5,3],5],[0,0]]
+[3,5]
+[[2,6],5]
+[[5,[[6,0],3]],[[3,[8,7]],[2,0]]]
+HERE
+
+# data = <<HERE
+# [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+# [[[5,[2,8]],4],[5,[[9,9],0]]]
+# [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+# [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+# [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+# [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+# [[[[5,4],[7,7]],8],[[8,3],8]]
+# [[9,3],[[9,9],[6,[4,9]]]]
+# [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+# [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+# HERE
+
+# data = <<HERE
+# [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+# [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+# [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+# [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+# [7,[5,[[3,8],[1,4]]]]
+# [[2,[2,2]],[8,[8,1]]]
+# [2,9]
+# [1,[[[9,3],9],[[9,0],[0,7]]]]
+# [[[5,[7,4]],7],1]
+# [[[[4,2],2],6],[8,7]]
+# HERE
+
+# data = <<HERE
+# [1,1]
+# [2,2]
+# [3,3]
+# [4,4]
+# [5,5]
+# [6,6]
+# HERE
+
+# data = <<HERE
+# [[[[4,3],4],4],[7,[[8,4],9]]]
+# [1,1]
+# HERE
+
+class Snailfish
+  attr_accessor :left, :right, :parent
+
+  def initialize(left, right, parent = nil)
+    @left = left
+    @right = right
+    @parent = parent
+  end
+
+  def reduce
+    while left.explode!(:left) || right.explode!(:right) || left.split! || right.split!
+      # no-op :)
+    end
+    self
+  end
+
+  def explode!(dir, depth = 1)
+    return true if Snailfish === left && left.explode!(:left, depth + 1)
+    return true if Snailfish === right && right.explode!(:right, depth + 1)
+
+    if Integer === left && Integer === right && depth >= 4
+      add(left, :left)
+      add(right, :right)
+
+      self.parent.public_send("#{dir}=", 0)
+
+      return true
+    end
+
+    false
+  end
+
+  def add(num, direction)
+    seen = []
+    current = self
+
+    loop do
+      # Reached root node
+      break if current.parent.nil?
+
+      seen << current
+      if seen.size == 1 || seen.include?(current.public_send(direction))
+        current = current.parent
+      else
+        if Integer === current.public_send(direction)
+
+        else
+          current = current.public_send(direction)
+          direction = { left: :right, right: :left }[direction]
+
+          until Integer === current.public_send(direction)
+            current = current.public_send(direction)
+          end
+        end
+
+        current.public_send("#{direction}=", current.public_send(direction) + num)
+
+        # We added our number!
+        return
+      end
+    end
+
+    if seen.include?(current.public_send(direction))
+      # Throw away number
+      return
+    end
+
+    other_direction = { left: :right, right: :left }[direction]
+    current = current.public_send(direction)
+    until Integer === current.public_send(other_direction)
+      current = current.public_send(other_direction)
+    end
+    current.public_send("#{other_direction}=", current.public_send(other_direction) + num)
+  end
+
+  def split!
+    did_split = false
+
+    case left
+    when Integer
+      if left >= 10
+        self.left = Snailfish.new(left.fdiv(2).floor, left.fdiv(2).ceil, self)
+        did_split = true
+      end
+    when Snailfish
+      did_split = left.split!
+    end
+
+    return true if did_split
+
+    case right
+    when Integer
+      if right >= 10
+        self.right = Snailfish.new(right.fdiv(2).floor, right.fdiv(2).ceil, self)
+        did_split = true
+      end
+    when Snailfish
+      did_split = right.split!
+    end
+
+    did_split
+  end
+
+  def +(other)
+    num = Snailfish.new(self, other, nil)
+    self.parent = num
+    other.parent = num
+
+    num.reduce
+  end
+
+  def magnitude
+    left_magnitude = Integer === left ? left : left.magnitude
+    right_magnitude = Integer === right ? right : right.magnitude
+    3 * left_magnitude + 2 * right_magnitude
+  end
+
+  def to_s
+    [left.to_s, right.to_s]
+  end
+end
+
+def to_snailfish(elem, parent = nil)
+  if elem.is_a?(Array)
+    a, b = elem
+    root = Snailfish.new(nil, nil, parent)
+    left = to_snailfish(a, root)
+    right = to_snailfish(b, root)
+    root.left = left
+    root.right = right
+
+    root
+  else
+    elem
+  end
+end
+
+snailfish_numbers = data.lines.map { |line| to_snailfish(eval(line.chomp)) }
+
+puts snailfish_numbers.reduce { |acc, num| acc + num }.magnitude
+puts (data.lines.map(&:chomp).permutation(2).map do |(a, b)|
+  x = to_snailfish(eval(a))
+  y = to_snailfish(eval(b))
+  (x + y).magnitude
+end.max)
