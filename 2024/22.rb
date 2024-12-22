071b561da20c925529e991b11045169d98f9da9b
Mads Ohm Larsen <mads.ohm@gmail.com>
2024-12-22 18:32:25 +0100

    Clean up December 22

--- 2024/22.rb
+++ 2024/22.rb
@@ -7,27 +7,25 @@ INPUT
 
 input = DATA.read
 
-global_changes = {}
-result = input.split("\n").map do |secret_num|
-  secret_num = secret_num.to_i
-
+global_changes = Hash.new(0)
+result = input.split("\n").map(&:to_i).map do |secret_num|
   bananas = [secret_num % 10]
 
   2000.times do
-    secret_num = ((secret_num * 64) ^ secret_num) % 16777216
-    secret_num = ((secret_num / 32) ^ secret_num) % 16777216
-    secret_num = ((secret_num * 2048) ^ secret_num) % 16777216
+    secret_num = ((secret_num << 6) ^ secret_num) % 16777216
+    secret_num = ((secret_num >> 5) ^ secret_num) % 16777216
+    secret_num = ((secret_num << 11) ^ secret_num) % 16777216
 
     bananas << secret_num % 10
   end
 
   changes = bananas.each_cons(2).map { |a, b| b - a }
 
-  seen = {}
+  seen = Set.new
   changes.each_cons(4).with_index do |(a, b, c, d), idx|
-    next if seen[[a, b, c, d]]
-    seen[[a, b, c, d]] = true
-    global_changes[[a, b, c, d]] ||= 0
+    next if seen.include?([a, b, c, d])
+    seen << [a, b, c, d]
+
     global_changes[[a, b, c, d]] += bananas[idx+4]
   end
 
